daceb1d0894f5218c3a62cb7508224b6f8fe2698
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2025-10-27 21:01:22 -0300

    add initial entity and SQL setup for project management

---
+++ backend/src/main/java/br/com/gabrielferreira/users/entities/ProjectEntity.java
@@ -0,0 +1,34 @@
+package br.com.gabrielferreira.users.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString(exclude = "roles")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "TB_PROJECT")
+public class ProjectEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 5818954769587702218L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "project")
+    private List<RoleEntity> roles = new ArrayList<>();
+}
---
+++ backend/src/main/java/br/com/gabrielferreira/users/entities/UserEntity.java
@@ -0,0 +1,72 @@
+package br.com.gabrielferreira.users.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString(exclude = "project")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "TB_USER", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_EMAIL_PROJECT", columnNames = {"EMAIL", "PROJECT_ID"})
+})
+public class UserEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3382588111235172319L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name = "EMAIL", nullable = false)
+    private String email;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "USER_EXTERNAL_ID", nullable = false, unique = true)
+    private UUID userExternalId;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "TB_USER_ROLE",
+            joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID", table = "TB_USER"),
+            inverseJoinColumns = @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID", table = "TB_ROLE"))
+    private List<RoleEntity> roles = new ArrayList<>();
+
+    @JoinColumn(name = "PROJECT_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_USER_PROJECT_ID"))
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ProjectEntity project;
+
+    @CreationTimestamp
+    @Column(name = "CREATED_AT", nullable = false, updatable = false, columnDefinition = "datetime")
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "UPDATED_AT", columnDefinition = "datetime")
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    public void generateUserExternalId() {
+        this.userExternalId = UUID.randomUUID();
+    }
+}
